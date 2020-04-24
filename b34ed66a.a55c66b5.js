3419bf5a3543999e4d74cca3db54c6de57557fab
sedufau <stephane.dufau@univ-amu.fr>
2020-04-24 23:15:15 +0200

    Deploy website version based on

--- b34ed66a.a55c66b5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{117:function(o){o.exports=JSON.parse('{"allTagsPath":"/online-xp-docs/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/online-xp-docs/blog/tags/hello"}')}}]);
--- b8285adc.9672abc4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{118:function(o){o.exports=JSON.parse('{"allTagsPath":"/online-xp-docs/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/online-xp-docs/blog/tags/facebook"}')}}]);
